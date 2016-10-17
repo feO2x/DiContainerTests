171a026f7a6a1541dc0d8c3c51a4005eb69373cd
Kenny Pflug <kenny.pflug@live.de>
2016-10-17 02:27:58 +0200

    added example for DryIoc

---
+++ DryIocTests/Class1.cs
@@ -0,0 +1,30 @@
+﻿using DryIoc;
+using FluentAssertions;
+using SampleTypes;
+using Xunit;
+
+namespace DryIocTests
+{
+    public class TestsForDryIoc : BaseTest
+    {
+        private readonly Container _container;
+
+        public TestsForDryIoc()
+        {
+            _container = new Container();
+            _container.Register<ISampleInterface, SampleClass>();
+            _container.RegisterInstance(typeof(int), Value2);
+            _container.RegisterInstance(typeof(string), Value1);
+        }
+
+        [Fact]
+        public void ResolveWithoutParameters()
+        {
+            var instance = _container.Resolve<ISampleInterface>();
+            instance.Value1.Should().Be(Value1);
+            instance.Value2.Should().Be(Value2);
+        }
+
+        // Cannot override parameters in DryIoc
+    }
+}
